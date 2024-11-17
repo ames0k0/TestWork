4f7dc93a82927679f24308c59d091eecad808537
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2024-11-17 19:06:16 +0300

    Saving changes

--- WebDevelopment/CREOS-Play/web/weather/services/YandexWeather.py
+++ WebDevelopment/CREOS-Play/web/weather/services/YandexWeather.py
@@ -1,7 +1,6 @@
 import datetime
+import random
 
-import requests
-from bs4 import BeautifulSoup
 
 from ..models import City, CityWeather
 from . import OpenStreetMap
@@ -11,19 +10,33 @@ YANDEX_WEATHER_URI = "https://yandex.ru/pogoda/ru-RU/details?lat={LATITUDE}&lon=
 YANDEX_WEATHER_RENEW_EVERY_MIN = 30
 
 
-def request_weather(city):
-    session = requests.Session()
-    response = session.get(
-        YANDEX_WEATHER_URI.format(
-            LATITUDE=city.latitude,
-            LONGITUDE=city.longitude,
-        )
+def request_weather(city) -> tuple[str, dict | None]:
+    _ = YANDEX_WEATHER_URI.format(
+        LATITUDE=city.latitude,
+        LONGITUDE=city.longitude,
     )
-    response
+    # XXX: make request
+    return "", {
+        "degrees_celsius": random.randint(-10, 10),
+        "atmospheric_pressure": random.randint(10, 30),
+        "wind_speed": random.randint(100, 1000),
+    }
 
 
 def init_city_weather(city: City) -> tuple[str, CityWeather | None]:
-    pass
+    error, weather_response = request_weather(city)
+    if error:
+        return error, weather_response
+
+    city_weather = CityWeather(
+        degrees_celsius=weather_response["degrees_celsius"],
+        atmospheric_pressure=weather_response["atmospheric_pressure"],
+        wind_speed=weather_response["wind_speed"],
+        city=city,
+    )
+    city_weather.save()
+
+    return error, city_weather
 
 
 def get_weather(city_name: str) -> tuple[str, CityWeather | None]:
--- WebDevelopment/CREOS-Play/web/weather/views.py
+++ WebDevelopment/CREOS-Play/web/weather/views.py
@@ -1,10 +1,26 @@
+from http.client import HTTPException
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from .services import YandexWeather
 
 
 def weather(request):
-  city_weather = ...
+  if request.method != "GET":
+    raise HTTPException(f"Method \"{request.method}\" not supported!")
+
+  city_name = request.GET.get("city")
+  if not city_name:
+    raise HTTPException("Missing `city` argument!")
+
+  error, city_weather = YandexWeather.get_weather(city_name)
+  if error:
+    raise HTTPException(error)
+
+  return JsonResponse(
+    city_weather
+  )
 
 
 def requests():
